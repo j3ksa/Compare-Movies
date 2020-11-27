1af0cdef5ea55b7eb37c4d7fcd100072908e512a
Łukasz Jęksa <lukasz.jeksa@gmail.com>
2020-11-27 01:15:35 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '196ce44',
+            s: searchTerm
+        }
+    });
+
+    console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+let timeoutId;
+const onInput = event => {
+    if (timeoutId){
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() =>{
+        fetchData(event.target.value)
+    }, 500)
+};
+    
+    
+input.addEventListener('input', onInput );
+;
